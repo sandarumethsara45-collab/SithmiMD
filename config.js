e7f70cab3f84080ae5b9a7e7d8cf9b8f2be7f228
sandarumethsara45-collab <sandarumethsara45@gmail.com>
2026-04-22 01:24:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/sandarumethsara45-collab/SithmiMD/blob/main/images/Sithmi-MD.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 SITHMI-MD Is Alive Now😍*",
+BOT_OWNER: '94743320676',  // Replace with the owner's phone number
 
 
 
